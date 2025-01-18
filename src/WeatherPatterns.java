18ef00eae366a7d1356a7dbb3cb6a78858658084
soccer47 <stevie.halprin@menloschool.org>
2025-01-18 11:43:27 -0800

    commit

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -17,23 +17,20 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
 
-    // Adjacency matrix representing edges of graph at edges[from][to]
+    // Adjacency list with a list of the edges leading to each vertex at edges[vertex]
     static ArrayList<Integer>[] edges;
-    // Array to keep track of the longest path from each node
+    // Array to keep track of the longest path ending at each node
     static int[] longestPaths;
 
     public static int longestWarmingTrend(int[] temperatures) {
-
-        // Initialize the adjacency matrix (the array of edges)
+        // Initialize the adjacency list (the array of edges)
         edges = new ArrayList[temperatures.length];
-
         // Initialize all the ArrayLists in the edges array
         for (int i = 0; i < edges.length; i++) {
             edges[i] = new ArrayList<>();
         }
         // Initialize the array of longest paths
         longestPaths = new int[temperatures.length];
-
         // Iterate through the temperatures array to fill the edges array
         for (int i = 0; i < temperatures.length; i++) {
             // Iterate through every temperature that has been passed already
@@ -44,32 +41,31 @@ public class WeatherPatterns {
                 }
             }
         }
-
         // Initialize the Integer to hold the longest recorded run to 0
         int longestRun = 0;
         // Integer to hold the length of the longest run ending at the current vertex
         int currentRun;
-
-        for (int i = 0; i < edges.length; i++) {
+        // Find the longest path ending at each node and return the longest one
+        // Start at the last node to reduce excess alteration of longestRun
+        for (int i = edges.length - 1; i >= 0; i--) {
             currentRun = longestPathTo(i);
             // If the current vertex ends a longer run than the longest previously recorded run, update longestRun
             if (currentRun > longestRun) {
                 longestRun = currentRun;
             }
         }
-
         // Return the longest run
         return longestRun;
     }
 
-    // Method to return the longest path ending at a given vertex
+    // Method to return the longest path ending at a given vertex using recursion
     public static int longestPathTo(int vertex) {
-        // Initialize the length of the path to 0
-        int len = 0;
         // If the current node has already been visited, just return the longest path ending at this node
         if (longestPaths[vertex] != 0) {
             return longestPaths[vertex];
         }
+        // Initialize the length of the longest path ending at the current vertex to 0
+        int len = 0;
         // Recursively call longestPathTo on each of the vertices that leads to this vertex
         // Set len to the longest found path ending at a parent vertex
         for (int nextNode : edges[vertex]) {
@@ -79,7 +75,9 @@ public class WeatherPatterns {
                 len = longestPath;
             }
         }
+        // Add the longest path ending at the current node to the array for quicker future calls
         longestPaths[vertex] = len + 1;
+        // Return the longest path ending at the current vertex
         return len + 1;
     }
 }
